19278e392d57c4363bc516c4a149c71d6ece88d6
samsung <samsung@samsung-PC>
2015-09-26 14:40:18 -0500

    Daniel Gallego - 26/09/2015

---
+++ src/pcsupport/PCSupport.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pcsupport;
+
+import interfaz.Inicio;
+
+/**
+ *
+ * @author samsung
+ */
+public class PCSupport {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Inicio in = new Inicio();
+        in.setVisible(true);
+    }
+}
